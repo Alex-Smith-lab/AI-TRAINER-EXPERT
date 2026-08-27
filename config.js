af87296900442830cf1d92586a9b6e34fd723e27
Alex-Smith-lab <antonymbali96@gmail.com>
2026-08-27 18:18:12 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.ATE_CONFIG = {
-  SUPABASE_URL: "https://mlmldgwzvkprpwkmfdlh.supabase.co",
-
-  SUPABASE_PUBLISHABLE_KEY:
-    "sb_publishable_8WucrYYIhnr1EXdNMkdMsQ_vFtZecB2",
-
-  COMPANY_NAME: "AI TRAINER EXPERT"
-};
