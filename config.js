ef33772373c3311648b336086f6be1dc0e432f6f
Alex-Smith-lab <antonymbali96@gmail.com>
2026-08-27 16:58:22 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.ATE_CONFIG = {
+  SUPABASE_URL: "https://mlmldgwzvkprpwkmfdlh.supabase.co",
+
+  SUPABASE_PUBLISHABLE_KEY:
+    "sb_publishable_8WucrYYIhnr1EXdNMkdMsQ_vFtZecB2",
+
+  COMPANY_NAME: "AI TRAINER EXPERT"
+};
